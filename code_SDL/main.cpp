a99e0856129a6bfb34ae22d20349040d5fcbbcbb
leamous <lea.mousel@telecom-sudparis.eu>
2015-04-08 18:50:57 +0200

    test SDL
    fenêtre dégradée façon jacky !

---
+++ code_SDL/main.cpp
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL.h>
+
+void pause ();
+
+int main(int argc, char *argv[])
+{
+    SDL_Surface *ecran = NULL, *lignes[256] = {NULL};
+    SDL_Rect position;
+    int i = 0;
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    ecran = SDL_SetVideoMode(640, 256, 32, SDL_HWSURFACE);
+
+    for (i = 0 ; i <= 255 ; i++)
+        lignes[i] = SDL_CreateRGBSurface(SDL_HWSURFACE, 640, 1, 32, 0, 0, 0, 0);
+
+    SDL_WM_SetCaption("JACKKKY", NULL);
+
+    SDL_FillRect(ecran, NULL, SDL_MapRGB(ecran->format, 255, 0, 112));
+
+    for (i = 0 ; i <= 255 ; i++)
+    {
+        position.x = 0; // Les lignes sont à gauche (abscisse de 0)
+        position.y = i; // La position verticale dépend du numéro de la ligne
+        SDL_FillRect(lignes[i], NULL, SDL_MapRGB(ecran->format, i, 0,0 ));
+        SDL_BlitSurface(lignes[i], NULL, ecran, &position);
+    }
+
+    SDL_Flip(ecran);
+    pause();
+
+    for (i = 0 ; i <= 255 ; i++) // N'oubliez pas de libérer les 256 surfaces
+        SDL_FreeSurface(lignes[i]);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
+}
+void pause()
+{
+    int continuer = 1;
+    SDL_Event event;
+
+    while (continuer)
+    {
+        SDL_WaitEvent(&event);
+        switch(event.type)
+        {
+            case SDL_QUIT:
+                continuer = 0;
+        }
+    }
+}
